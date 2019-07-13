aa32745e3f4795336e0238c81f9025b3a434b292
shuja_akbar <shujaakbar1415@gmail.com>
2019-07-13 14:16:23 +0000

    Creating Admin or super user

--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 import datetime
 from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
 
