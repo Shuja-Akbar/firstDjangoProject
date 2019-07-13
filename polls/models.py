c0b69abb4ee85c862e670992b37f87fce9cc6d64
shuja_akbar <shujaakbar1415@gmail.com>
2019-07-13 22:54:07 +0000

    Edited the file models.py to fix the bug for test cases

--- polls/models.py
+++ polls/models.py
@@ -13,7 +13,10 @@ from django.utils.encoding import python_2_unicode_compatible
 class Question(models.Model):
     # ...
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     # ...
     def __str__(self):
