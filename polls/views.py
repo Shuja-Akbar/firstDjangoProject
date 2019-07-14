3d19f17cdf28fbfa3a0b28c8703dee0a93b4c0f2
shuja_akbar <shujaakbar1415@gmail.com>
2019-07-14 06:26:24 +0000

    Here I added style.css file to Customize the look of webpage

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.template import loader
 
 from django.http import Http404
+from django.utils import timezone
 
 from django.shortcuts import render
 
@@ -25,12 +26,20 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()
+                                        ).order_by('-pub_date')[:5]
+        #return Question.objects.order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
